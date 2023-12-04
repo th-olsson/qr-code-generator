18af81f6434f6a2ea46fda234f3aaaecac869e49
Thomas Olsson <olss.thom@gmail.com>
2023-12-04 11:58:37 +0100

    updates favicon and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { QrCodeProvider } from "@/context/qr-context";
-import { DownloadOptionsProvider } from "@/context/download-options-context";
+import { OptionsProvider } from "@/context/options-context";
 import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "QR Code Generator",
-  description: "Create QR codes for free - download as PNG or SVG",
+  title: "ZQR.se - QR Code Generator",
+  description: "Create QR codes for free. Save as PNG or SVG",
 };
 
 export default function RootLayout({
@@ -28,10 +28,10 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <QrCodeProvider>
-            <DownloadOptionsProvider>
+            <OptionsProvider>
               {children}
               <Analytics />
-            </DownloadOptionsProvider>
+            </OptionsProvider>
           </QrCodeProvider>
         </ThemeProvider>
       </body>
